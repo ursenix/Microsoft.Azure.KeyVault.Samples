b845e8ff08f8df10f48e0615ef1baa152270f017
Senthil Kumaran <ursenix@gmail.com>
2017-07-05 16:57:51 +0530

    Add project files.

---
+++ samples/SampleAzureWebService/SampleKeyVaultClientWebRole/Constants.cs
@@ -0,0 +1,17 @@
+﻿//
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License. See License.txt in the project root for
+// license information.
+//
+
+namespace SampleKeyVaultClientWebRole
+{
+    internal static class Constants
+    {
+        public static string StorageAccountKeySecretUrlSetting = "StorageAccountKeySecretUrl";
+        public static string StorageAccountNameSetting = "StorageAccountName";
+        public static string KeyVaultAuthClientIdSetting = "KeyVaultAuthClientId";
+        public static string KeyVaultAuthCertThumbprintSetting = "KeyVaultAuthCertThumbprint";
+        public static string KeyVaultSecretCacheDefaultTimeSpan = "KeyVaultSecretCacheDefaultTimeSpan";
+    }
+}
